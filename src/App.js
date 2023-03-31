7083415f6431db5bec095089074e1555e68cb9af
Paul Bridi <paulbridi@gmail.com>
2023-04-01 00:30:18 +0100

    add upload button attempting to call backend api endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [response, setResponse] = useState('');
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    fetch('http://localhost:5000/api/video', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => setResponse(data.message))
+    .catch(error => console.error(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {response && <p>{response}</p>}
     </div>
   );
 }
